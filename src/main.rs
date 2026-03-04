1d88b8ff8a36eeda0bfdd980c134e922d0f7a3b6
GermanViter <german.viter@icloud.com>
2026-03-03 23:17:11 -0500

    start new version with read_to_string to follow tutorial

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,33 @@ fn main() {
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
 
-    //generate_lines(pattern, path);
-    out_put_file(path, pattern);
+    generate_lines(pattern, path);
+    //out_put_file(path, pattern);
 }
 
-//fn generate_lines(pattern: String, path: String) {
-//    let args = Cli::parse();
-//    let reader = std::fs::read_to_string(path).expect("could not read file");
-//
+fn generate_lines(pattern: String, path: String) {
+   let args = Cli::parse();
+   let reader = std::fs::read_to_string(path);
+
+   match reader {
+       Ok(content) => { println!("File content: {}", content); }
+       Err(error) => { println!("Oh noes: {}", error); }
+   }
+}
+
+//fn out_put_file(path: String, pattern: String) -> std::io::Result<()> {
+//    let file = File::open(path)?;
+//    let reader = BufReader::new(file);
+//    
 //    for line in reader.lines() {
-//        if line.contains(&args.pattern) {
-//            println!("{}", line);
+//        let line_content = line?;
+//        if line_content.contains(&pattern) {
+//            match line_content {
+//                Ok(content) => { println!("Result: {content}"); }
+//                Err(error) => { println!("oh noes: {error}"); }
+//            }
 //        }
 //    }
+//
+//    Ok(())
 //}
-
-fn out_put_file(path: String, pattern: String) -> std::io::Result<()> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-    
-    for line in reader.lines() {
-        let line_content = line?;
-        if line_content.contains(&pattern) {
-            println!("{line_content}");
-        }
-    }
-
-    Ok(())
-}
