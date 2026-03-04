ba75e0221024458010f11301b573f1918d9b55d7
GermanViter <german.viter@icloud.com>
2026-03-04 12:42:25 -0500

    copy paste final result from tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,25 @@
+use anyhow::{Context, Result};
 use clap::Parser;
-use std::io::{BufReader, BufRead};
-use std::fs::File;
-
 
 #[derive(Parser)]
+
 struct Cli {
     pattern: String,
     path: std::path::PathBuf,
 }
 
-fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
+fn main() -> Result<()> {
+    let args = Cli::parse();
 
-    generate_lines(pattern, path);
-    //out_put_file(path, pattern);
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
-
-fn generate_lines(pattern: String, path: String) -> Result<(), Box<dyn std::error::Error>> {
-   let args = Cli::parse();
-   let reader = std::fs::read_to_string(path)?;
-
-   println!("file content: {:?}", reader);
-   Ok(())
-}
-
-//fn out_put_file(path: String, pattern: String) -> std::io::Result<()> {
-//    let file = File::open(path)?;
-//    let reader = BufReader::new(file);
-//    
-//    for line in reader.lines() {
-//        let line_content = line?;
-//        if line_content.contains(&pattern) {
-//            match line_content {
-//                Ok(content) => { println!("Result: {content}"); }
-//                Err(error) => { println!("oh noes: {error}"); }
-//            }
-//        }
-//    }
-//
-//    Ok(())
-//}
 
